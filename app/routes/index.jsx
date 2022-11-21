100f51c3dfe1480da6ab33289a86d1d174379484
Stefan G <stefg@stefans-air.mynetworksettings.com>
2022-11-20 19:42:53 -0500

    Added the Link from @remix-run/react

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,10 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
+    <>
     <h1>Remix Project 2022</h1>
+    <Link to="/demo">Go to demo page</Link>
+    </>
   );
 }
